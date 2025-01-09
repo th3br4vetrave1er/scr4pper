b45a0934087055fae364101827fcbd23935de98a
th3br4vetrave1er <tazdingodan@gmail.com>
2025-01-10 00:03:18 +0200

    add minimal fastapi server for test

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-'Hello World'
+from fastapi import FastAPI
+
+# declaring fastapi object
+
+app = FastAPI()
+
+# implementing testing endpoint
+
+@app.get('/')
+async def root():
+    return {'message': 'Hello World'}
+
